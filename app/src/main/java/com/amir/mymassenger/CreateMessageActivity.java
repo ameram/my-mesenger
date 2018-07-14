5b93053434f490f7807efbb04eaa5edba53e3987
Amir <playergood0@live.com>
2018-07-14 07:45:14 +0430

    button for opening another app

--- app/src/main/java/com/amir/mymassenger/CreateMessageActivity.java
+++ app/src/main/java/com/amir/mymassenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,10 +20,12 @@ public class CreateMessageActivity extends Activity {
     public void onSendMessage(View view) {
         EditText messageText = findViewById(R.id.message);
         String messageTextString = messageText.getText().toString();
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageTextString);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageTextString);
         startActivity(intent);
-
+//Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageTextString);
 
     }
 }
